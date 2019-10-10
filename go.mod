63220b6b6fc5be19cc7930417aa2cd74d194eb8c
Noam <ben.zichri@gmail.com>
2019-10-10 13:28:28 +0300

    create empty CLI project

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/PaperBag42/gud
+
+go 1.12
+
+require github.com/spf13/cobra v0.0.5
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/PaperBag42/gud/cmd"
+
+func main() {
+	cmd.Execute()
+}
