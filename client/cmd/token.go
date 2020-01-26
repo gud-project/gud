f8abb4e10d524ae40f92bb407321faa127e207c0
nitaiby <nitaiben30@gmail.com>
2020-01-26 09:41:37 +0200

    add signup command

---
+++ client/cmd/token.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+const tokenPath = "path"
+
+func saveToken(name, token string) error{
+	f, err := os.Create(filepath.Join(tokenPath, name))
+	if err != nil {
+		return fmt.Errorf("Failed to create token file: %s\n", err.Error())
+	}
+	defer f.Close()
+
+	_, err = f.Write([]byte(token))
+	if err != nil {
+		return fmt.Errorf("Failed to write token: %s\n", err.Error())
+	}
+	return nil
+}
