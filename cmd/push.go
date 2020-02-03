7f3022d64b4943bd6dfd5115dc33124a63199d90
nitaiby <nitaiben30@gmail.com>
2020-02-03 19:29:52 +0200

    add pull command

--- cmd/push.go
+++ cmd/push.go
@@ -1,18 +1,3 @@
-/*
-Copyright © 2020 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
@@ -26,7 +11,7 @@ import (
 // pushCmd represents the push command
 var pushCmd = &cobra.Command{
 	Use:   "push [branch]",
-	Short: "A brief description of your command",
+	Short: "Push current branch to server",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
@@ -59,7 +44,7 @@ to quickly create a Cobra application.`,
 			println(err)
 			return
 		}
-		req.AddCookie(&http.Cookie{Name: "ds_user_id", Value: data})
+		req.AddCookie(&http.Cookie{Name: "session", Value: data})
 		client := &http.Client{}
 		resp, err := client.Do(req)
 		if err != nil {
