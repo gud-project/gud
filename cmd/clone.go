0e8086ec15dd71e792c6adaddae351811a7f4328
Noam Ben-zichri <ben.zichri@gmail.com>
2020-04-03 20:30:57 +0300

    change clone to accept owner and project name

--- cmd/clone.go
+++ cmd/clone.go
@@ -16,13 +16,16 @@ limitations under the License.
 package cmd
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
 	"gitlab.com/magsh-2019/2/gud/gud"
+	"net/http"
 )
 
 // cloneCmd represents the clone command
 var cloneCmd = &cobra.Command{
-	Use:   "clone <URL>",
+	Use:   "clone [domain] <owner> <project>",
+	Args:  cobra.RangeArgs(2, 3),
 	Short: "Create a copy of a project in the server",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -31,20 +34,16 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		var domain string
-		if err := checkArgsNum(0, len(args), modeMax) ; err == nil {
+		var domain, owner, project string
+		if len(args) == 2 {
 			var gConf gud.GlobalConfig
-			err = gud.LoadConfig(&gConf, gConf.GetPath())
+			err := gud.LoadConfig(&gConf, gConf.GetPath())
 			if err != nil {
 				return err
 			}
-			domain = gConf.ServerDomain
+			domain, owner, project = gConf.ServerDomain, args[0], args[1]
 		} else {
-			if len(args) == 1 {
-				domain = args[0]
-			} else {
-				return err
-			}
+			domain, owner, project = args[0], args[1], args[2]
 		}
 
 		p, err := gud.StartHeadless("")
@@ -52,12 +51,54 @@ to quickly create a Cobra application.`,
 			return err
 		}
 
-		err = PullBranch(p, domain)
+		var gConfig gud.GlobalConfig
+		err = gud.LoadConfig(&gConfig, gConfig.GetPath())
 		if err != nil {
 			return err
 		}
 
-		return p.AddHead()
+		req, err := http.NewRequest(http.MethodGet,
+			fmt.Sprintf("http://%s/api/v1/user/%s/project/%s/pull?branch=%s",
+				domain, owner, project, gud.FirstBranchName), nil)
+		if err != nil {
+			return err
+		}
+
+		req.AddCookie(&http.Cookie{Name: "session", Value: gConfig.Token})
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+		err = checkResponseError(resp)
+		if err != nil {
+			return err
+		}
+
+		err = p.PullBranch(gud.FirstBranchName, resp.Body, resp.Header.Get("Content-Type"))
+		if err != nil {
+			return err
+		}
+		err = p.AddHead()
+		if err != nil {
+			return err
+		}
+
+		var config gud.Config
+		err = p.LoadConfig(&config)
+		if err != nil {
+			return err
+		}
+
+		config.OwnerName = owner
+		config.ProjectName = project
+		err = p.WriteConfig(config)
+		if err != nil {
+			return err
+		}
+
+		return p.Reset()
 	},
 }
 
